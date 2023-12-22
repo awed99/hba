0d747c9a5992591f575bf38d717f08a2533db93a
Dewa Danu Brata <dewadanubrata@gmail.com>
2023-12-23 00:19:20 +0700

    fixing error build vx3

---
+++ .next/server/4.js
@@ -0,0 +1,11 @@
+exports.ids = [4];
+exports.modules = {
+
+/***/ "OFRp":
+/***/ (function(module, exports, __webpack_require__) {
+
+if (false) {}
+
+/***/ })
+
+};;
---
+++ .next/server/5.js
@@ -0,0 +1,20 @@
+exports.ids = [5];
+exports.modules = {
+
+/***/ "IxrA":
+/***/ (function(module, exports, __webpack_require__) {
+
+/*!
+ * Isotope PACKAGED v3.0.6
+ *
+ * Licensed GPLv3 for open source use
+ * or Isotope Commercial License for commercial use
+ *
+ * https://isotope.metafizzy.co
+ * Copyright 2010-2018 Metafizzy
+ */
+if (false) {}
+
+/***/ })
+
+};;
